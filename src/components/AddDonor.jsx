ff5a5f41344eab8b4d6ec3041b2146015c4a1483
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-13 09:46:02 +0530

    Add Component Done

--- src/components/AddDonor.jsx
+++ src/components/AddDonor.jsx
@@ -55,7 +55,7 @@ const AddDonor = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                        <div className="row">
+                        <div className="row g-3">
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <label htmlFor="" className="form-label">Donar Name: </label>
